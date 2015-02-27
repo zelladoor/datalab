1bd2a75b9148bbc8f87eaa050ea704ac9eaed3bd
Drew Bryant <bryantd@google.com>
2015-02-27 08:44:10 -0800

    Adds worksheet editor controller methods for moving cells up/down via action emitter

--- sources/server/src/ui/scripts/app/components/worksheeteditor/WorksheetEditorDirective.ts
+++ sources/server/src/ui/scripts/app/components/worksheeteditor/WorksheetEditorDirective.ts
@@ -17,6 +17,7 @@
  * Directive for rendering a single notebook
  */
 /// <reference path="../../../../../../../../externs/ts/angularjs/angular.d.ts" />
+/// <amd-dependency path="app/components/actionemitter/ActionEmitter" />
 /// <amd-dependency path="app/components/cellinserter/CellInserterDirective" />
 /// <amd-dependency path="app/components/codecell/CodeCellDirective" />
 /// <amd-dependency path="app/components/markdowncell/MarkdownCellDirective" />
@@ -28,8 +29,61 @@ import _app = require('app/App');
 
 var log = logging.getLogger(constants.scopes.worksheetEditor);
 
-interface NotebookScope extends ng.IScope {
+interface WorksheetEditorScope extends ng.IScope {
   worksheet?: app.notebook.Worksheet;
+  ctrl?: WorksheetEditorController;
+}
+
+class WorksheetEditorController {
+  _actionEmitter: app.IActionEmitter;
+  _scope: WorksheetEditorScope;
+
+  static $inject = ['$scope', constants.actionEmitter.name];
+  constructor (scope: WorksheetEditorScope, actionEmitter: app.IActionEmitter) {
+    this._actionEmitter = actionEmitter;
+    this._scope = scope;
+
+    this._scope.ctrl = this;
+  }
+
+  /**
+   * Emits a move cell (up) action to move the cell at the given index
+   */
+  moveCellUp (cellIndexToMove: number) {
+    var cell = this._scope.worksheet[cellIndexToMove];
+    switch (cellIndexToMove) {
+      case 0:
+        // Cannot move cell up, already at top of worksheet (no-op)
+        return;
+      case 1:
+        // Move the cell to the top of the worksheet.
+        // Because there is not currently an insertBefore option for the move cell action
+        // a insertAfter value of null is used to indicate inserting at the head of the worksheet
+        actionEmitter.moveCel(this._scope.worksheet.id, cell.id, null);
+      break;
+      default:
+        // Given worksheet cell ids [A, B, C, D], if we want to move C "up" (towards beginning of
+        // worksheet), we want to insert cell C after cell A, which is two before cell C
+        var insertAfterCell = this._scope.worksheet.cells[cellIndexToMove - 2];
+        actionEmitter.moveCel(this._scope.worksheet.id, cell.id, insertAfterCell.id);
+    }
+  }
+
+  /**
+   * Emits a move cell (down) action to move the cell at the given index
+   */
+  moveCellDown (cellIndexToMove: number) {
+    var cell = this._scope.worksheet[cellIndexToMove];
+
+    if (cellIndexToMove == (this._scope.worksheet.cells.length - 1)) {
+      // Then this the cell to move is already last, so no-op
+      return;
+    }
+
+    // Insert the current cell after the next cell
+    var nextCell = this._scope.worksheet.cells[cellIndexToMove + 1];
+    actionEmitter.moveCell(this._scope.worksheet.id, cell.id, nextCell.id);
+  }
 }
 
 function worksheetEditorDirective (): ng.IDirective {
@@ -39,6 +93,7 @@ function worksheetEditorDirective (): ng.IDirective {
       worksheet: '='
     },
     replace: true,
+    controller: WorksheetEditorController,
     templateUrl: constants.scriptPaths.app + '/components/worksheeteditor/worksheeteditor.html',
   }
 }
