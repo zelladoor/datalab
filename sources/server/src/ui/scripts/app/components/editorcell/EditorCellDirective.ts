e3864029c18325a717e7e78ecd48ca555a1f699c
Drew Bryant <bryantd@google.com>
2015-04-01 15:54:51 -0700

    Adds docs and stricter typing to editor cell directive

--- sources/server/src/ui/scripts/app/components/editorcell/EditorCellDirective.ts
+++ sources/server/src/ui/scripts/app/components/editorcell/EditorCellDirective.ts
@@ -31,17 +31,19 @@ import _app = require('app/App');
 var log = logging.getLogger(constants.scopes.editorCell);
 
 interface EditorCellScope extends ng.IScope {
+  // Externally assignable scope attributes.
   cell: app.notebooks.Cell;
+  enableEditRegion: boolean;
+  enablePreviewRegion: boolean;
+  getKeymap: Function;
+  onPreviewRegionDoubleClick: Function;
   worksheetId: string;
-  getKeymap?: any;
-  enableEditRegion?: boolean;
-  enablePreviewRegion?: boolean;
-  onPreviewRegionDoubleClick?: Function;
-  clientNotebookSession?: app.IClientNotebookSession;
 
-  actions?: any;
+  // Internally assigned scope attributes.
+  actions?: app.Map<Function>;
   active?: boolean;
-  keymap?: any;
+  clientNotebookSession?: app.IClientNotebookSession;
+  keymap?: app.Map<Function>;
 }
 
 class EditorCellController {
@@ -50,7 +52,14 @@ class EditorCellController {
   _scope: EditorCellScope;
 
   static $inject = ['$scope', constants.clientNotebookSession.name];
-  constructor (scope: EditorCellScope, clientNotebookSession: app.IClientNotebookSession) {
+
+  /**
+   * Constructor.
+   *
+   * @param scope The directive's scope.
+   * @param clientNotebookSession Client's notebook session object.
+   */
+  constructor(scope: EditorCellScope, clientNotebookSession: app.IClientNotebookSession) {
     this._scope = scope;
 
     scope.active = false;
@@ -59,63 +68,94 @@ class EditorCellController {
     scope.clientNotebookSession = clientNotebookSession;
   }
 
-  // handle events that occur on the editor instance
-  _createActionHandlers () {
+  /**
+   * Creates the set of DOM event callbacks to register on the editor DOM element.
+   *
+   * @return Map of DOM event name to callback.
+   */
+  _createActionHandlers(): app.Map<Function> {
     return {
+      // When the contained code editor element is focused, respond by activating this cell.
       'focus': this.activate.bind(this)
     }
   }
 
-  activate () {
+  /**
+   * Makes this cell have the "active" status.
+   */
+  activate() {
     var that = this;
     this._scope.$evalAsync(() => {
+      // The currently active cell is maintained by the session.
+      //
+      // Request that this cell become the currently selected cell, which will deselect any other
+      // cell that is currently selected (i.e., cell selection is mutually exclusive).
       that._scope.clientNotebookSession.selectCell(that._scope.cell);
     });
   }
 
-  deactivate () {
+  /**
+   * Removes the "active" status from this cell.
+   */
+  deactivate() {
     // TODO(bryantd): Support programmatic cell deactivation.
     // Need to listen for blur event on the outer-most element of the editor cell
     // (the containing div) and use that event as the deactivate trigger.
     //
-    // Then, invoke the nbdata.deactivateCell() method here
+    // Then, invoke the nbdata.deactivateCell() method here.
+    //
+    // Note: a currently active cell becomes inactive whenever any other cell becomes active.
+    // That is, like radio buttons, the "active" status is mutually exclusive among the cells in a
+    // notebook, for a given client's notebook session.
   }
 
 }
 
-function editorCellDirectiveLink (
+/**
+ * Editor cell directive link function.
+ *
+ * @param scope Editor cell directive scope.
+ * @param element Editor cell directive DOM element.
+ * @param attrs The set of attributes populated on the directive.
+ * @param controller Controller for the directive.
+ */
+function editorCellDirectiveLink(
     scope: EditorCellScope,
     element: ng.IAugmentedJQuery,
-    attrs: any,
+    attrs: ng.IAttributes,
     controller: EditorCellController
     ): void {
 
+  // Watch the active cell (session global) for changes. If this cell becomes the currently
+  // assigned active cell, then propagate this "active" status to the directive scope.
   scope.$watch(() => {
     var activeCell = scope.clientNotebookSession.activeCell;
     // Avoid having the watch recursively compare all of the data within the cell by
     // returning the cell id as the watched value. If there is no active cell, any constant
     // sentinel value that is not also a valid cell id can be returned (using undefined here).
     return (activeCell && activeCell.id) || undefined;
-  }, (newValue: any, oldValue: any) => {
+  }, (activeCellId: any) => {
       // Check to see if the cell for this directive has become active and update the scope
       // with the current active/inactive status.
-      scope.active = (newValue === scope.cell.id);
+      scope.active = (activeCellId === scope.cell.id);
   });
 }
 
 /**
- * Creates a directive definition.
+ * Creates an editor cell directive definition.
+ *
+ * @return A directive definition.
  */
-function editorCellDirective (): ng.IDirective {
+function editorCellDirective(): ng.IDirective {
   return {
     restrict: 'E',
     scope: {
       cell: '=',
-      worksheetId: '=',
       getKeymap: '&keymap',
       enableEditRegion: '=',
       enablePreviewRegion: '=',
-      onPreviewRegionDoubleClick: '&'
+      onPreviewRegionDoubleClick: '&',
+      worksheetId: '='
     },
     templateUrl: constants.scriptPaths.app + '/components/editorcell/editorcell.html',
     replace: true,
